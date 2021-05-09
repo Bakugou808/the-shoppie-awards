5d95659916cd7408e1b50337b3b6d42ba6577e02
Janu <janu.m.sung@gmail.com>
2021-05-09 11:35:52 -0700

    updated api key

--- src/Services/api.jsx
+++ src/Services/api.jsx
@@ -2,6 +2,7 @@ const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
 const URL = `http://www.omdbapi.com/?apikey=${API_KEY}&s=`;
 
 export async function fetchSearchResults(param) {
+  console.log("url", URL);
   const resp = await fetch(`${URL}${param}`);
 
   return await resp.json();
